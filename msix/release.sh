e53e0ecd3c10a55bfaf3d7fdb2097d50b72dd15a
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-07-26 14:25:39 +0200

    msix: start aligning the process to the other installers'
    Git for Windows already has the established pattern that you call a
    `release.sh` script in the respective subdirectory to generate an
    installer, portable Git, MinGit, etc.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- msix/release.sh
+++ msix/release.sh
@@ -1,41 +1,72 @@
-function Write-PackageLayoutFile {
-    param ([string]$version, [string]$filename) 
+#!/bin/sh
 
-    $packageLayout = [xml]@"
-<PackagingLayout xmlns="http://schemas.microsoft.com/appx/makeappx/2017">
-  <PackageFamily ID="Git-$($version)" FlatBundle="false" ManifestPath="appxmanifest.xml" ResourceManager="false">
-    <Package ID="Git-x64-$($version)" ProcessorArchitecture="x64">
-      <Files>
-        <File DestinationPath="Git\**" SourcePath="..\build\x64\**" />
-        <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
-        <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
-      </Files>
-    </Package>
-    <Package ID="Git-x86-$($version)" ProcessorArchitecture="x86">
-      <Files>
-        <File DestinationPath="Git\**" SourcePath="..\build\x86\**" />
-        <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
-        <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
-      </Files>
-    </Package>
-    <!-- <Package ID="Git-arm64-$($version)" ProcessorArchitecture="arm64">
-      <Files>
-        <File DestinationPath="Git\**" SourcePath="arm64\**" />
-        <File DestinationPath="Images\*.png" SourcePath="..\build-extra\msix\Images\*.png" />
-        <File DestinationPath="Public\Fragments\*" SourcePath="..\build-extra\msix\Fragments\*" />
-      </Files>
-    </Package> -->
-  </PackageFamily>
-</PackagingLayout>
-"@
-    $packageLayout.Save($filename)
-
+die () {
+	echo "$*" >&2
+	exit 1
 }
 
-function Write-AppxManifest {
-    param ([string]$version, [string]$publisher, [string]$filename)
+# Evaluate architecture
+ARCH="$(uname -m)"
 
-    $appxmanifest = [xml]@"
+case "$ARCH" in
+i686)
+	die "i686 not yet supported"
+	;;
+x86_64)
+	BITNESS=64
+	;;
+*)
+	die "Unhandled architecture: $ARCH"
+	;;
+esac
+
+makeappx='/c/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/makeappx.exe'
+publisher="CN=82A13EFD-FE37-4EFC-8BA4-1C3E9EFE5F23"
+
+version="$(powershell -Command "Write-Host (Get-Item '$(cygpath -aw /cmd/git.exe)').VersionInfo.FileVersionRaw")"
+test -n "$version" || die "Could not figure out version of /cmd/git.exe"
+
+cd "$(dirname "$0")" || die "Could not switch to working directory"
+
+echo "Generating release notes to be included in the installer ..."
+../render-release-notes.sh --css usr/share/git/ ||
+die "Could not generate release notes"
+
+etc_gitconfig="$(git -c core.editor=echo config --system -e 2>/dev/null)" &&
+etc_gitconfig="$(cygpath -au "$etc_gitconfig")" &&
+etc_gitconfig="${etc_gitconfig#/}" ||
+die "Could not determine the path of the system config"
+
+echo "Generating file list to be included in the installer ..."
+LIST="$(printf '%s\n' git-bash.exe cmd/git-gui.exe cmd/gitk.exe cmd/git.exe)" ||
+LIST="$(ARCH=$ARCH BITNESS=$BITNESS \
+	ETC_GITCONFIG="$etc_gitconfig" \
+	PACKAGE_VERSIONS_FILE=package-versions.txt \
+	sh ../make-file-list.sh)" ||
+die "Could not generate file list"
+
+cat >PackagingLayout.xml <<EOF || die "Could not write PackagingLayout.xml"
+<PackagingLayout xmlns="http://schemas.microsoft.com/appx/makeappx/2017">
+  <PackageFamily ID="Git-$version" FlatBundle="false" ManifestPath="appxmanifest.xml" ResourceManager="false">
+    <Package ID="Git-x64-$version" ProcessorArchitecture="x64">
+      <Files>
+        <File DestinationPath="Git\\etc\\package-versions.txt" SourcePath="package-versions.txt" />
+        <File DestinationPath="Git\\ReleaseNotes.html" SourcePath="..\\ReleaseNotes.html" />
+$(
+	root="$(cygpath -aw / | sed 's|\\|&&|g')"
+	echo "$LIST" | sed -e 's|/|\\|g' \
+		-e '/\\WebView2Loader.dll$/d' \
+		-e 's|^\(.*\)$|        <File DestinationPath="Git\\\1" SourcePath="'"$root"'\\\1" />|' \
+)
+        <File DestinationPath="Images\\*.png" SourcePath="Images\\*.png" />
+        <File DestinationPath="Public\\Fragments\\*" SourcePath="Fragments\\*" />
+      </Files>
+    </Package>
+  </PackageFamily>
+</PackagingLayout>
+EOF
+
+cat >appxmanifest.xml <<EOF || die "Could not write appxmanifest.xml"
 <?xml version="1.0" encoding="utf-8"?>
 
 <Package xmlns="http://schemas.microsoft.com/appx/manifest/foundation/windows10"
@@ -49,12 +80,12 @@ function Write-AppxManifest {
   xmlns:desktop6="http://schemas.microsoft.com/appx/manifest/desktop/windows10/6"
   xmlns:iot2="http://schemas.microsoft.com/appx/manifest/iot/windows10/2" IgnorableNamespaces="mp uap uap3 uap5 rescap desktop desktop4 desktop6 iot2">
 
-  <Identity Name="JohannesSchindelin.Git" Publisher="$($publisher)" Version="$($version)" ProcessorArchitecture="neutral"/>
+  <Identity Name="JohannesSchindelin.Git" Publisher="$publisher" Version="$version" ProcessorArchitecture="neutral"/>
 
   <Properties>
     <DisplayName>Git</DisplayName>
     <PublisherDisplayName>Johannes Schindelin</PublisherDisplayName>
-    <Logo>Images\StoreLogo.png</Logo>
+    <Logo>Images\\StoreLogo.png</Logo>
   </Properties>
 
   <Dependencies>
@@ -67,25 +98,25 @@ function Write-AppxManifest {
   </Resources>
 
   <Applications>
-    <Application Id="GitBash" Executable="Git\git-bash.exe" EntryPoint="Windows.FullTrustApplication">
+    <Application Id="GitBash" Executable="Git\\git-bash.exe" EntryPoint="Windows.FullTrustApplication">
       <uap:VisualElements
         DisplayName="Git Bash"
         Description="Git Bash"
         BackgroundColor="transparent"
-        Square150x150Logo="Images\Square150x150Logo.png"
-        Square44x44Logo="Images\Square44x44Logo.png">
+        Square150x150Logo="Images\\Square150x150Logo.png"
+        Square44x44Logo="Images\\Square44x44Logo.png">
         <uap:DefaultTile
-          Wide310x150Logo="Images\Wide310x150Logo.png"
+          Wide310x150Logo="Images\\Wide310x150Logo.png"
           ShortName="Git Bash"
-          Square71x71Logo="Images\SmallTile.png"
-          Square310x310Logo="Images\LargeTile.png">
+          Square71x71Logo="Images\\SmallTile.png"
+          Square310x310Logo="Images\\LargeTile.png">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
             <uap:ShowOn Tile="square310x310Logo"/>
           </uap:ShowNameOnTiles>
         </uap:DefaultTile >
-        <uap:SplashScreen Image="Images\SplashScreen.png" />
+        <uap:SplashScreen Image="Images\\SplashScreen.png" />
       </uap:VisualElements>
       <Extensions>
         <uap3:Extension Category="windows.appExecutionAlias" Executable="Git/git-bash.exe" EntryPoint="Windows.FullTrustApplication">
@@ -99,25 +130,25 @@ function Write-AppxManifest {
         </uap3:Extension>
       </Extensions>
     </Application>
-    <Application Id="GitGui" Executable="Git\cmd\git-gui.exe" EntryPoint="Windows.FullTrustApplication">
+    <Application Id="GitGui" Executable="Git\\cmd\\git-gui.exe" EntryPoint="Windows.FullTrustApplication">
       <uap:VisualElements
         DisplayName="Git GUI"
         Description="Git GUI"
         BackgroundColor="transparent"
-        Square150x150Logo="Images\Square150x150Logo.png"
-        Square44x44Logo="Images\Square44x44Logo.png">
+        Square150x150Logo="Images\\Square150x150Logo.png"
+        Square44x44Logo="Images\\Square44x44Logo.png">
         <uap:DefaultTile
-          Wide310x150Logo="Images\Wide310x150Logo.png"
+          Wide310x150Logo="Images\\Wide310x150Logo.png"
           ShortName="Git GUI"
-          Square71x71Logo="Images\SmallTile.png"
-          Square310x310Logo="Images\LargeTile.png">
+          Square71x71Logo="Images\\SmallTile.png"
+          Square310x310Logo="Images\\LargeTile.png">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
             <uap:ShowOn Tile="square310x310Logo"/>
           </uap:ShowNameOnTiles>
         </uap:DefaultTile >
-        <uap:SplashScreen Image="Images\SplashScreen.png" />
+        <uap:SplashScreen Image="Images\\SplashScreen.png" />
       </uap:VisualElements>
       <Extensions>
         <uap3:Extension Category="windows.appExecutionAlias" Executable="Git/cmd/git-gui.exe" EntryPoint="Windows.FullTrustApplication">
@@ -127,26 +158,26 @@ function Write-AppxManifest {
         </uap3:Extension>
       </Extensions>
     </Application>
-    <Application Id="Git" Executable="Git\cmd\git.exe" EntryPoint="Windows.FullTrustApplication">
+    <Application Id="Git" Executable="Git\\cmd\\git.exe" EntryPoint="Windows.FullTrustApplication">
       <uap:VisualElements
         DisplayName="Git"
         Description="Git"
         BackgroundColor="transparent"
-        Square150x150Logo="Images\Square150x150Logo.png"
-        Square44x44Logo="Images\Square44x44Logo.png"
+        Square150x150Logo="Images\\Square150x150Logo.png"
+        Square44x44Logo="Images\\Square44x44Logo.png"
         AppListEntry="none">
         <uap:DefaultTile
-          Wide310x150Logo="Images\Wide310x150Logo.png"
+          Wide310x150Logo="Images\\Wide310x150Logo.png"
           ShortName="Git"
-          Square71x71Logo="Images\SmallTile.png"
-          Square310x310Logo="Images\LargeTile.png">
+          Square71x71Logo="Images\\SmallTile.png"
+          Square310x310Logo="Images\\LargeTile.png">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
             <uap:ShowOn Tile="square310x310Logo"/>
           </uap:ShowNameOnTiles>
         </uap:DefaultTile >
-        <uap:SplashScreen Image="Images\SplashScreen.png" />
+        <uap:SplashScreen Image="Images\\SplashScreen.png" />
       </uap:VisualElements>
       <Extensions>
         <uap3:Extension Category="windows.appExecutionAlias" Executable="Git/cmd/git.exe" EntryPoint="Windows.FullTrustApplication">
@@ -156,26 +187,26 @@ function Write-AppxManifest {
         </uap3:Extension>
       </Extensions>
     </Application>
-    <Application Id="Gitk" Executable="Git\cmd\gitk.exe" EntryPoint="Windows.FullTrustApplication">
+    <Application Id="Gitk" Executable="Git\\cmd\\gitk.exe" EntryPoint="Windows.FullTrustApplication">
       <uap:VisualElements
         DisplayName="Gitk"
         Description="Gitk"
         BackgroundColor="transparent"
-        Square150x150Logo="Images\Square150x150Logo.png"
-        Square44x44Logo="Images\Square44x44Logo.png"
+        Square150x150Logo="Images\\Square150x150Logo.png"
+        Square44x44Logo="Images\\Square44x44Logo.png"
         AppListEntry="none">
         <uap:DefaultTile
-          Wide310x150Logo="Images\Wide310x150Logo.png"
+          Wide310x150Logo="Images\\Wide310x150Logo.png"
           ShortName="Gitk"
-          Square71x71Logo="Images\SmallTile.png"
-          Square310x310Logo="Images\LargeTile.png">
+          Square71x71Logo="Images\\SmallTile.png"
+          Square310x310Logo="Images\\LargeTile.png">
           <uap:ShowNameOnTiles>
             <uap:ShowOn Tile="square150x150Logo"/>
             <uap:ShowOn Tile="wide310x150Logo"/>
             <uap:ShowOn Tile="square310x310Logo"/>
           </uap:ShowNameOnTiles>
         </uap:DefaultTile >
-        <uap:SplashScreen Image="Images\SplashScreen.png" />
+        <uap:SplashScreen Image="Images\\SplashScreen.png" />
       </uap:VisualElements>
       <Extensions>
         <uap3:Extension Category="windows.appExecutionAlias" Executable="Git/cmd/gitk.exe" EntryPoint="Windows.FullTrustApplication">
@@ -191,6 +222,6 @@ function Write-AppxManifest {
     <rescap:Capability Name="runFullTrust" />
   </Capabilities>
 </Package>
-"@
-    $appxmanifest.Save($filename)
-}
+EOF
+
+"$makeappx" build -o -f PackagingLayout.xml -op . -pv "$version" -bv "$version"
